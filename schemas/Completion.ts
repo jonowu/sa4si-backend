d4caf864105c14a0de3f9b9d4e307d80c3476e8f
Jonathan Wu <53251695+jonowu@users.noreply.github.com>
2021-05-11 23:23:34 +1000

    fix: completion date defaults to now

--- schemas/Completion.ts
+++ schemas/Completion.ts
@@ -5,7 +5,7 @@ export const Completion = list({
   fields: {
     action: relationship({ ref: 'Action' }),
     user: relationship({ ref: 'User.completions' }),
-    completionDate: timestamp(),
+    completionDate: timestamp({ defaultValue: () => new Date().toISOString() }),
     kudos: relationship({ ref: 'User', many: true }),
   },
 });
