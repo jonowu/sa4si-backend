6eb16331ba77a631ab97939f9bc2fe6ee74aeefb
Jonathan Wu <53251695+jonowu@users.noreply.github.com>
2021-05-17 16:02:24 +1000

    fix: set default user for completion to currently signed in user

--- schemas/Completion.ts
+++ schemas/Completion.ts
@@ -1,10 +1,19 @@
 import { list } from '@keystone-next/keystone/schema';
 import { timestamp, relationship } from '@keystone-next/fields';
+import { isSignedIn } from '../access';
 
 export const Completion = list({
+  access: {
+    create: isSignedIn,
+  },
   fields: {
     action: relationship({ ref: 'Action' }),
-    user: relationship({ ref: 'User.completions' }),
+    user: relationship({
+      ref: 'User.completions',
+      defaultValue: ({ context }) => ({
+        connect: { id: context.session.itemId },
+      }),
+    }),
     completionDate: timestamp({ defaultValue: () => new Date().toISOString() }),
     kudos: relationship({ ref: 'User', many: true }),
   },
